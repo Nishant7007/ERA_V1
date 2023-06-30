96d81e0788ad81448187522022b3c686e63d727b
Nishant Gupta <nishantguptahpu@gmail.com>
2023-06-30 14:54:02 +0530

    Add files via upload

---
+++ Session_8 _Batch_Normalization_And_Regularization/README.md
@@ -0,0 +1,81 @@
+# ASSIGNMENT
+
+### Assignment is:
+#### 1. Change the dataset to CIFAR10
+#### 2. Make this network:
+        2.1 C1 C2 c3 P1 C3 C4 C5 c6 P2 C7 C8 C9 GAP C10
+        Keep the parameter count less than 50000
+        Try and add one layer to another
+        Max Epochs is 20
+#### 3. You are making 3 versions of the above code (in each case achieve above 70% accuracy):
+        Network with Group Normalization
+        Network with Layer Normalization
+        Network with Batch Normalization
+#### 4. Share these details
+        Training accuracy for 3 models
+        Test accuracy for 3 models
+#### 5. Find 10 misclassified images for the BN model, and show them as a 5x2 image matrix in 3 separately annotated images. 
+        
+#### 6. write an explanatory README file that explains:
+        what is your code all about,
+        your findings for normalization techniques,
+        add all your graphs
+        your collection-of-misclassified-images 
+#### 7. Upload your complete assignment on GitHub and share the link on LMS
+
+
+
+### Solution:
+The main code is in this [notebook](https://github.com/Nishant7007/ERA_V1/blob/main/Session_8%20_Batch_Normalization_And_Regularization/main.ipynb)
+
+Following are the accuracies for all the three normalisaton techniques:
+
+| **Normalisation**     | **Number of parameters** | **Train Accuracy** | **Test Accuracy** | 
+|-----------------------|--------------------------|--------------------|-------------------|
+| Batch Normalisation   | 38,458                   | 74.77%             | 74.05%            |
+| Layer Normalisation   | 38,458                   | 72.53%             | 72.91%              |
+| Group Normalisation   | 38,458                   |                    |                   |
+####
+    Following are some of the sample images:
+ ![Getting Started](sample_images.png)   
+    
+#### Batch Normalisation: 
+    we started with Batch Normalisation. Following are the results of last 10 epochs
+ ![Getting Started](BN_epochs.png)
+####
+     We got training accuracy of 74.77% and test accuracy of 74.05%
+####
+    Following are the accuracy and loss plots for training and test set
+ ![Getting Started](performance_BN.png)
+####
+    Following are some of the misclassified images
+ ![Getting Started](misclassified_images_BN.png)
+
+
+#### Layer Normalisation: 
+    Then we tried Batch Normalisation. Following are the results of last 10 epochs
+ ![Getting Started](LN_epochs.png)
+####
+     We got training accuracy of 72.53% and test accuracy of 72.91%
+####
+    Following are the accuracy and loss plots for training and test set
+ ![Getting Started](performance_LN.png)
+####
+    Following are some of the misclassified images
+ ![Getting Started](misclassified_images_LN.png)
+
+
+#### Group Normalisation: 
+    we then tried Group Normalisation. Following are the results of last 10 epochs
+ ![Getting Started](GN_epochs.png)
+####
+     We got training accuracy of 17.38% and test accuracy of 18.50%
+####
+    Following are the accuracy and loss plots for training and test set
+ ![Getting Started](performance_GN.png)
+####
+    Following are some of the misclassified images
+ ![Getting Started](misclassified_images_GN.png)  
+    
+    
+ 
---
+++ Session_8 _Batch_Normalization_And_Regularization/train_test_models.py
@@ -0,0 +1,92 @@
+from __future__ import print_function
+from tqdm import tqdm
+import torch.optim as optim
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from torch.optim.lr_scheduler import StepLR
+
+class Train_Test_With_LR_Scheduler:
+    def __init__(self):
+        self.train_losses = []
+        self.test_losses = []
+        self.train_acc = []
+        self.test_acc = []
+        self.misclassified_images = []
+        self.original_labels = []
+        self.predicted_labels = []
+
+    def train(self, model, device, train_loader, optimizer, epoch):
+        model.train()
+        pbar = tqdm(train_loader)
+        correct = 0
+        processed= 0
+        for batch_idx, (data, target) in enumerate(pbar):
+            data, target = data.to(device), target.to(device)
+
+            optimizer.zero_grad()
+
+            y_pred = model(data)
+
+            loss = F.nll_loss(y_pred, target)
+            self.train_losses.append(loss)
+
+            loss.backward()
+            optimizer.step()
+
+            pred = y_pred.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            processed += len(data)
+
+            pbar.set_description(desc=f'loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+            self.train_acc.append(100*correct/processed)
+
+    def test(self, model, device, test_loader):
+        model.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in test_loader:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+                test_loss += F.nll_loss(output, target, reduction='sum').item()
+                pred = output.argmax(dim=1, keepdim=True)
+                indices = [item[0] for item in (pred.eq(target.view_as(pred))==False)]
+                mis_img = data[indices,:,:,:]
+                self.misclassified_images.extend(mis_img)
+                self.original_labels.extend(target[indices])
+                self.predicted_labels.extend(pred[indices])
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+            test_loss /= len(test_loader.dataset)
+            self.test_losses.append(test_loss)
+
+            print('\n Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+                test_loss, correct, len(test_loader.dataset),
+                100. * correct / len(test_loader.dataset)))
+
+            self.test_acc.append(100. * correct / len(test_loader.dataset))
+
+    def train_and_test_model(self, model, device, epochs, train_loader, test_loader):
+        model = model.to(device)
+        optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+        scheduler = StepLR(optimizer, step_size=6, gamma=0.1)
+        for epoch in range(epochs):
+            print(f'EPOCH: {epoch}')
+            self.train(model, device, train_loader, optimizer, epoch)
+            scheduler.step()
+            self.test(model, device, test_loader)
+        return self.train_losses, self.test_losses, self.train_acc, self.test_acc
+
+class ResultVisualisation:
+    def plot_accuracy_and_loss(self, train_losses, test_losses, train_acc, test_acc):
+        fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+        axs[0, 0].plot([loss.cpu().detach().numpy() for loss in train_losses])
+        axs[0, 0].set_title("Training Loss")
+        axs[1, 0].plot(train_acc)
+        axs[1, 0].set_title("Training Accuracy")
+        axs[0, 1].plot(test_losses)
+        axs[0, 1].set_title("Test Loss")
+        axs[1, 1].plot(test_acc)
+        axs[1, 1].set_title("Test Accuracy")
